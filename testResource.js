621661d279e95212df721cb4d7dfbff9ca86b7b0
nheise <n(dot)heise(at)heise(minus)media(dot)de>
2011-05-01 13:46:49 +0200

    change to Resource Object

--- testResource.js
+++ testResource.js
@@ -7,37 +7,36 @@ var uriPattern = require("../../lib/URIPattern.js");
 
 var responseLoader = require('../../lib/ResponseLoader.js').responseLoader;
 
-exports.resources = [ {
-    uriPattern : uriPattern.create( '/testResource/{id}' ),
-    'GET' : {
-        'text/html' : loadIndex
-    }
-}, {
-    uriPattern : uriPattern.create( '/testResource/image/{filename}' ),
-    'GET' : {
-        'image/png' : responseLoader[mediaType.IMAGE_PNG],
-        '*/*' : responseLoader[mediaType.IMAGE_PNG]
-    }
-}, {
-    uriPattern : uriPattern.create( '/testResource/css/{filename}' ),
-    'GET' : {
-        'text/css' : responseLoader[mediaType.TEXT_CSS]
-    }
-}, {
-    uriPattern : uriPattern.create( '/testResource/js/{filename}' ),
-    'GET' : {
-        'text/javascript' : responseLoader[mediaType.TEXT_JAVASCRIPT],
-        '*/*' : responseLoader[mediaType.TEXT_JAVASCRIPT]
-    }
-}, {
-    uriPattern : uriPattern.create( '/testResource/video/{filename}' ),
-    'GET' : {
-        'video/ogg' : responseLoader[mediaType.VIDEO_OGG],
-        'text/html' : responseLoader[mediaType.VIDEO_OGG],
-        'video/mp4' : responseLoader[mediaType.VIDEO_MP4],
-        '*/*' : responseLoader[mediaType.VIDEO_MP4]
-    }
-} ];
+var resourceFactory = require("../../lib/ResourceFactory.js");
+
+var indexResource = resourceFactory.createResource( '/testResource/{id}' );
+indexResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.TEXT_HTML, loadIndex );
+
+var imageResource = resourceFactory.createResource( '/testResource/image/{filename}' );
+imageResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.IMAGE_PNG, responseLoader[mediaType.IMAGE_PNG] );
+imageResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.ALL, responseLoader[mediaType.IMAGE_PNG] );
+
+var cssResource = resourceFactory.createResource( '/testResource/css/{filename}' );
+cssResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.TEXT_CSS, responseLoader[mediaType.TEXT_CSS] );
+
+var jsResource = resourceFactory.createResource( '/testResource/js/{filename}' );
+jsResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.TEXT_JAVASCRIPT, responseLoader[mediaType.TEXT_JAVASCRIPT] );
+jsResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.ALL, responseLoader[mediaType.TEXT_JAVASCRIPT] );
+
+var videoResource = resourceFactory.createResource( '/testResource/video/{filename}' );
+videoResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.VIDEO_OGG, responseLoader[mediaType.VIDEO_OGG] );
+videoResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.TEXT_HTML, responseLoader[mediaType.VIDEO_OGG] );
+videoResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.VIDEO_MP4, responseLoader[mediaType.VIDEO_MP4] );
+videoResource.addMediaTypeAndResponseLoaderForRequestMethodGET( mediaType.ALL, responseLoader[mediaType.VIDEO_MP4] );
+
+exports.resources = [         
+    indexResource,
+    imageResource,
+    cssResource,
+    jsResource,
+    videoResource
+];
+
 
 //var path = require('path');
 
